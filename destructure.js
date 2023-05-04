257b25998706937af9ca5c87800dead4c0333829
Fanfan durand <durand90@live.com>
2023-05-04 15:04:08 -0400

    five

--- destructure.js
+++ destructure.js
@@ -1,4 +1,6 @@
 
+
+/*
 //1
 const cars = ['Tesla', 'Mercedes', 'Honda'];
 const [ randomCar ] =  cars;
@@ -9,3 +11,19 @@ console.log(otherRandomCar);
 //results
 //console.log(randomCar) //log: Tesla
 //console.log(otherRandomCar) //log: Mercedes
+*/
+
+
+//2
+const employee = {
+    name: 'elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+
+console.log(name);
+console.log(otherName);
+//results
+//console.log(name)  //log: elon
+//console.log(otherName) //log: elon

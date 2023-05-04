498098b26771db8344fea5cd858722d38aa50ca7
Fanfan durand <durand90@live.com>
2023-05-04 15:22:45 -0400

    ten

--- destructure.js
+++ destructure.js
@@ -61,3 +61,17 @@ console.log(first == third);
 //console.log(first == third) //log: false
 */
 
+//5
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ , willThisWork] = secondKey;
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+//results
+//c
